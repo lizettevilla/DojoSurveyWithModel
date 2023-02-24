4c12824447a73145a1dc5f45aeefc011af9b5ca3
lizettevilla <122892543+lizettevilla@users.noreply.github.com>
2023-02-24 13:25:51 -0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("survey")]
+    public IActionResult Submission(Survey yourSurvey)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Survey", yourSurvey);
+        }
+        else
+        {
+            return View("Index");
+        }
+     }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
